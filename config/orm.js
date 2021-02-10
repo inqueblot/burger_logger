cc92405b06a62695266a01c41d37dab97943d2f7
inqueblot <notminebydesign@gmail.com>
2021-02-10 17:12:47 -0600

    setup controller and server routes

--- config/orm.js
+++ config/orm.js
@@ -1,20 +1,24 @@
 const connection = require('./connection');
 
 const orm = {
-    selectAll: function () {
+    selectAll: function (cb) {
         connection.query("SELECT * FROM burgers", function (err, result) {
-            if (err) throw err;
+            if (err) {
+                throw err;
+            }
             console.log(result);
+            cb(result);
         });
     },
-    insertOne: function () {
+    insertOne: function (cb) {
         var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, FALSE)"
         connection.query(queryString, [req.body.burgers], function (err, data) {
             if (err) {
                 res.status(500).end();
             }
             console.log(data);
-        })
+            cb(data);
+        });
 
     },
     updateOne()
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,2 +1,13 @@
 const express = require('express');
+
+const router = express.Router();
+
 const burger = require('../models/burger');
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+
+    })
+})
+
+module.exports = router;
