e1815f8e31ba6d3e3ecb21dfc019a2be50778de5
inqueblot <notminebydesign@gmail.com>
2021-02-10 00:41:15 -0600

    set up orm layout

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,23 @@
+const connection = require('./connection');
+
 const orm = {
-    selectAll()
-    insertOne()
+    selectAll: function () {
+        connection.query("SELECT * FROM burgers", function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    insertOne: function () {
+        var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, FALSE)"
+        connection.query(queryString, [req.body.burgers], function (err, data) {
+            if (err) {
+                res.status(500).end();
+            }
+            console.log(data);
+        })
+
+    },
     updateOne()
-}
+};
+
+module.exports = orm;
